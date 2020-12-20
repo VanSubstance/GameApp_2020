40ad74781a91b6480e1838794cf073dd8d65ff9d
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-12-20 22:27:22 +0900

    에러 수정

--- server/AppGame2020/src/main/java/com/my/ag2020/controller/TestController.java
+++ server/AppGame2020/src/main/java/com/my/ag2020/controller/TestController.java
@@ -3,6 +3,7 @@ package com.my.ag2020.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class TestController {
 	}
 	
 	@RequestMapping(value = "/monster/type/{type}", method = RequestMethod.GET)
-	public List<MonsterVO> viewMonsterByType(String type) {
+	public List<MonsterVO> viewMonsterByType(@PathVariable String type) {
 		return service.viewMonsterByType(type);
 	}
 
